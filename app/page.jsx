cb19d08d48f6f8703363d123b3d56ed3d86c67ec
bizli189413-dev <bizli189413@gmail.com>
2026-05-30 01:45:04 +0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 20, textAlign: "center" }}>
       <h1>🎮 Game Livo Landing Page</h1>
-      <p>Site is working successfully</p>
+      <p>Site is working now</p>
     </main>
   );
 }
